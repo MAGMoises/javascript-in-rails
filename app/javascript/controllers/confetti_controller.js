48d8063fde711e8d4d76806e3372eb36cfc896a3
MAGMoises <moisesaraujog42@gmail.com>
2023-10-24 17:17:55 -0400

    update javascript-in-rails

---
+++ app/javascript/controllers/confetti_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import JSConfetti from 'js-confetti';
+
+// Connects to data-controller="confetti"
+export default class extends Controller {
+  connect() {}
+  fire() {
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti({
+      emojis: ['🍻', '🥃', '🍺', '🍖', '🍾', '🦴'],
+      confettiNumber: 100,
+      })
+  }
+}
